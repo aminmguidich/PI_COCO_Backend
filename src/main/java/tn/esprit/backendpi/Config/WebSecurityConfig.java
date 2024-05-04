c7fdd35383565b634c52b52f1b31927d3d4bd0a9
hssan861 <115433855+hssan861@users.noreply.github.com>
2024-05-04 10:52:12 +0100

    mergeMgidich

--- src/main/java/tn/esprit/backendpi/Config/WebSecurityConfig.java
+++ src/main/java/tn/esprit/backendpi/Config/WebSecurityConfig.java
@@ -15,17 +15,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import tn.esprit.backendpi.Security.Jwt.AuthEntryPointJwt;
 import tn.esprit.backendpi.Security.Jwt.AuthTokenFilter;
 import tn.esprit.backendpi.Security.Services.UserDetailsServiceImpl;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 
 
 
@@ -148,24 +142,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
 
         return http.build();
     }
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        ClientRegistration registration = ClientRegistration.withRegistrationId("google")
-                .clientId(clientId)
-                .clientSecret(clientSecret)
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("http://localhost:8080/login/oauth2/code/google") // Replace with your actual redirect URI
-                .scope("openid", "profile", "email")
-                .authorizationUri("https://accounts.google.com/o/oauth2/auth")
-                .tokenUri("https://www.googleapis.com/oauth2/v3/token")
-                .userInfoUri("https://openidconnect.googleapis.com/v1/userinfo")
-                .userNameAttributeName(IdTokenClaimNames.SUB)
-                .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
-                .clientName("Google")
-                .build();
-        return new InMemoryClientRegistrationRepository(registration);
-    }
+
 
 
 }
